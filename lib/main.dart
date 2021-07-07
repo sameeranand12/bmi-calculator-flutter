eb895bba3b7915075ee56f4e325ec9f95fc1421c
sameeranand12 <201951134@iiitvadodara.ac.in>
2021-07-07 15:26:29 +0530

    text color

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor:Color(0xFF0A0E21) ,
+        accentColor: Colors.purple,
+        textTheme: TextTheme(
+            bodyText2:TextStyle(color:Colors.white),)
+
+      ),
+
+      home: InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('BMI CALCULATOR')),
+      ),
+
+      body: Center(
+
+        child: Text('Body Text'),
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+ //vvs
