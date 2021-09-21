40369d416012d3590f04d179fdfdef71128520f2
sameeranand12 <201951134@iiitvadodara.ac.in>
2021-09-21 23:25:16 +0530

    change

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -10,7 +10,7 @@ class IconContent extends StatelessWidget {
   IconContent({this.icon , this.label});
   final IconData icon;
   final String label;
-  //j
+  //jjj
 
   @override
   Widget build(BuildContext context) {
