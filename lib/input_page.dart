dabc82aa454f61898f137ea139b483083c5021c7
sameeranand12 <201951134@iiitvadodara.ac.in>
2021-10-21 16:27:18 +0530

    slider

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -88,19 +88,28 @@ class _InputPageState extends State<InputPage> {
                       )
                     ],
                   ),
-                  Slider(
-                    value: height.toDouble(),
-                    activeColor: Color(0xFFEB1555),
-                    inactiveColor: Color(0xFF8D8E98),
-                    min: 120.0,
-                    max: 220.0,
-                    onChanged: (double newValue) {
-                      setState(() {
-                        height = newValue.round();
-                      });
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      inactiveTickMarkColor: Color(0xFF8D8E98),
+                      activeTrackColor: Color(0xFFEB1555),
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x29EB1555),
+                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0)
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
 
-                      // print(newValue);
-                    },
+                      min: 120.0,
+                      max: 220.0,
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.round();
+                        });
+
+                        // print(newValue);
+                      },
+                    ),
                   ),
                 ],
               ),
